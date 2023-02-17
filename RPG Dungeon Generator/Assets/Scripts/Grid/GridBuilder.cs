8afae8d97d2003afd2c86037b00d1c1a30253a50
Joseph Collard <jcollard@users.noreply.github.com>
2023-02-17 07:51:20 -0700

    feat: Implement new Map file format and generation.

--- RPG Dungeon Generator/Assets/Scripts/Grid/GridBuilder.cs
+++ RPG Dungeon Generator/Assets/Scripts/Grid/GridBuilder.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using System.Linq;
 
 namespace CaptainCoder.Dungeoneering
 {
     public class GridBuilder
     {
-        private GridCellDatabase _database;        
+        private GridCellDatabase _database;
 
         public GridBuilder(GridCellDatabase database)
         {
@@ -13,13 +14,46 @@ namespace CaptainCoder.Dungeoneering
 
         public void Build(string grid, Transform container)
         {
-            string[] rows = grid.Split();
+            string[] rows = grid.Split("\n").Select(s => s.TrimEnd()).ToArray();
+            GameObject floorContainer = new ("Floors");
+            floorContainer.transform.parent = container;
+            BuildFloors(rows, floorContainer.transform);
+            GameObject wallContainer = new ("Walls");
+            wallContainer.transform.parent = container;
+            wallContainer.transform.localPosition = new Vector3(-Grid.CellSize + 1, 0, -0.5f);
+            BuildWalls(rows, wallContainer.transform);
+        }
+
+        private void BuildWalls(string[] rows, Transform container)
+        {
             for (int r = 0; r < rows.Length; r++)
             {
-                for (int c = 0; c < rows[r].Length; c++)
+                // On even rows, we care about the odd columns
+                // On odd rows, we care about the even columns
+                int startColumn = r % 2 == 0 ? 1 : 0;
+                for (int c = startColumn; c < rows[r].Length; c += 2)
                 {
                     char ch = rows[r][c];
-                    GameObject obj = _database.Instantiate(ch, container);
+                    if (ch == ' ') { continue; }
+                    GameObject obj = _database.InstantiateWall(ch, container);
+                    float row = (r * .5f);
+                    float col = (c * .5f);
+                    obj.name = $"({row}, {col}) - {obj.name}";
+                    obj.transform.localPosition = new Vector3(row * Grid.CellSize, 0, col * Grid.CellSize);
+                }
+            }
+        }
+
+        private void BuildFloors(string[] rows, Transform container)
+        {
+            for (int r = 0; r < (rows.Length - 1) / 2; r++)
+            {
+                string row = rows[r * 2 + 1];
+                for (int c = 0; c < (row.Length - 1) / 2; c++)
+                {
+                    char ch = row[c * 2 + 1];
+                    if (ch == ' ') { continue; }
+                    GameObject obj = _database.InstantiateTile(ch, container);
                     obj.name = $"({r}, {c}) - {obj.name}";
                     obj.transform.localPosition = new Vector3(r * Grid.CellSize, 0, c * Grid.CellSize);
                 }
--- RPG Dungeon Generator/Assets/Scripts/Grid/GridCellDatabase.cs
+++ RPG Dungeon Generator/Assets/Scripts/Grid/GridCellDatabase.cs
@@ -6,25 +6,48 @@ namespace CaptainCoder.Dungeoneering
     public class GridCellDatabase : MonoBehaviour
     {
         [SerializeField]
-        private List<Entry> _entries;
-        private Dictionary<char, GameObject> _entryDict;
-        private Dictionary<char, GameObject> EntryDict
+        private List<Entry> _cellEntries;
+        private Dictionary<char, GameObject> _cellEntryDict;
+        private Dictionary<char, GameObject> CellEntryDict
         {
             get
             {
-                if (_entryDict == null)
+                if (_cellEntryDict == null)
                 {
-                    _entryDict = new Dictionary<char, GameObject>();
-                    foreach (Entry e in _entries)
+                    _cellEntryDict = new Dictionary<char, GameObject>();
+                    foreach (Entry e in _cellEntries)
                     {
-                        Debug.Assert(!_entryDict.ContainsKey(e.Key), $"Entry List contains duplicate entry for character '{e.Key}'");
-                        _entryDict[e.Key] = e.Value;
+                        Debug.Assert(!_cellEntryDict.ContainsKey(e.Key), $"Entry List contains duplicate entry for character '{e.Key}'");
+                        _cellEntryDict[e.Key] = e.Value;
                     }
                 }
-                return _entryDict;
+                return _cellEntryDict;
             }
         }
-        public GameObject Instantiate(char ch, Transform parent = null) => Instantiate(EntryDict[ch], parent);
+
+        
+        [SerializeField]
+        private List<Entry> _wallEntries;
+        private Dictionary<char, GameObject> _wallEntryDict;
+        private Dictionary<char, GameObject> WallEntryDict
+        {
+            get
+            {
+                if (_wallEntryDict == null)
+                {
+                    _wallEntryDict = new Dictionary<char, GameObject>();
+                    foreach (Entry e in _wallEntries)
+                    {
+                        Debug.Assert(!_wallEntryDict.ContainsKey(e.Key), $"Entry List contains duplicate entry for character '{e.Key}'");
+                        _wallEntryDict[e.Key] = e.Value;
+                    }
+                }
+                return _wallEntryDict;
+            }
+        }
+
+        public GameObject InstantiateTile(char ch, Transform parent = null) => Instantiate(CellEntryDict[ch], parent);
+        public GameObject InstantiateWall(char ch, Transform parent = null) => Instantiate(WallEntryDict[ch], parent);
     
         [System.Serializable]
         public class Entry
@@ -32,7 +55,7 @@ namespace CaptainCoder.Dungeoneering
             [field: SerializeField]
             public char Key { get; private set; }
             [field: SerializeField]
-            public GameObject Value { get; private set; }
+            public GameObject Value { get; private set; } 
         }
     }
 }
